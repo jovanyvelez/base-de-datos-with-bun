823f5e3288c30bf65aea29adf019386dacaa9fbb
jovany velez <jovany.velez@udea.edu.co>
2023-12-31 16:20:21 -0500

    estilo al inpunt

--- src/routes/pruebas/+page.svelte
+++ src/routes/pruebas/+page.svelte
@@ -3,13 +3,13 @@
 
 	//export let form;
 
-	const WITH = 200;
-
+	const WITH = 300;
+	let primera_imagen = true;
 	let input: HTMLInputElement;
-	
-	let imagen_to_upload;
-	let images: { id: number; file: string }[] = [];
 
+	let imagen_to_upload;
+	let imag: { id: number; main: boolean; file: string }[] = [];
+	$: images = [...imag];
 	let new_image_url = '';
 
 	let showImage = false;
@@ -57,12 +57,16 @@
 							context.drawImage(new_image, 0, 0, canvas.width, canvas.height);
 							new_image_url = context.canvas.toDataURL('image/jpeg', 100);
 						}
-						//imagen_to_upload =  urlToFIle(new_image_url)
+						//imagen_to_upload =  urlToFIle(new_image_url).relative.s-lavbwSbrVjO3
 					}
 					const existe = buscar_imagen(new_image_url);
-					if (!existe) images = [...images, { file: new_image_url, id: Date.now() }];
+					if (!existe)
+						images = [
+							...images,
+							{ file: new_image_url, id: Date.now(), main: primera_imagen ? true : false }
+						];
+					primera_imagen = false;
 					//const {data, error} = await grabar.storage.from('products').upload(`product_${Date.now()}.png`, imagen_to_upload)
-
 				};
 				new_image.remove();
 			});
@@ -91,22 +95,11 @@
 
 		return file;
 	};
-	// Función para convertir una URL de datos a Blob
-	function dataURItoBlob(dataURI: string) {
-		const byteString = atob(dataURI.split(',')[1]);
-		const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-		const ab = new ArrayBuffer(byteString.length);
-		const ia = new Uint8Array(ab);
 
-		for (let i = 0; i < byteString.length; i++) {
-			ia[i] = byteString.charCodeAt(i);
-		}
-
-		return new Blob([ab], { type: mimeString });
-	}
-
-	const deleteThis = (id: number) => {
+	const deleteThis = (main: boolean, id: number) => {
 		images = images.filter((item) => item.id != id);
+		if (images.length === 0) primera_imagen = true;
+		if (main) images[0].main = true;
 	};
 
 	const buscar_imagen = (imagen: string) => {
@@ -114,31 +107,66 @@
 
 		return temp_image ? true : false;
 	};
+
+	let selectedDiv = null;
+
+	function selectDiv(image: { id: number; main: boolean; file: string }) {
+		if (image.main) return;
+		image.main = true;
+		images.forEach((item) => {
+			if (item.id !== image.id) {
+				item.main = false;
+			} else {
+				item.main = true;
+			}
+		});
+		images = [...images];
+	}
 </script>
 
-<h1>Image Preview on File Upload</h1>
-<input bind:this={input} on:change={onChange} type="file" accept=".jpg, .jpeg, .png" />
+<main class="m-7 bg-white">
+	<h1 class="text-3xl font-semibold p-4">Agregar un producto</h1>
 
-<div  class="flex justify-center items-center p-2 flex-wrap">
-	{#if showImage}
-		{#each images as item (item.id)}
-			<div class="relative">
-				<img src={item.file} alt="{item.id}}" class="mx-2" />
-				<div
-					class="rounded-full bordered border-2 border-red-500 absolute top-2 right-4 cursor-pointer shadow-lg"
-				>
-					<button on:click={() => deleteThis(item.id)}
-						><span class=" text-xs hover:text-sm font-bold p-2 text-red-500">X</span></button
+	<article class="bordered boer- shadow-slate-400 shadow-lg p-4 rounded-xl">
+		<label for="laImagen">
+			<div class="flex ">
+				<div class="flex flex-col justify-center items-center max-w-md ">
+					<div
+						class="bordered border-4 border-sky-500 rounded-full w-9 h-9 flex justify-center items-center"
 					>
+						<span class="text-2xl my-4"> + </span>
+					</div>
+					<span class="text-xl font-semibold">Agregar </span>
 				</div>
 			</div>
-		{/each}
-	{:else}
-		<span>Image Preview</span>
-	{/if}
-</div>
+		</label>
+		<input
+			id="laImagen"
+			class="hidden"
+			bind:this={input}
+			on:change={onChange}
+			type="file"
+			accept=".jpg, .jpeg, .png .svg"
+		/>
 
-<!--form
+		<div class="flex justify-center items-center p-2 flex-wrap">
+			{#if showImage}
+				{#each images as item (item.id)}
+					<div class={`p-2 main relative ${item.main ? 'bordered border-4 border-red-500' : ''}`}>
+						<img src={item.file} alt="{item.id}}" class="mx-2" on:click={selectDiv(item)} />
+						<div
+							class="bg-white rounded-full bordered border-4 border-red-500 absolute top-2 right-4 cursor-pointer shadow-lg"
+						>
+							<button on:click={() => deleteThis(item.main, item.id)}
+								><span class=" text-sm hover:text-sm font-bold p-2 text-red-500">X</span></button
+							>
+						</div>
+					</div>
+				{/each}
+			{/if}
+		</div>
+
+		<!--form
 	action="?/registrar"
 	method="post"
 	enctype="multipart/form-data"
@@ -167,3 +195,5 @@
 	/>
 	<button type="submit" class="btn btn-primary">Enviar </button>
 </form-->
+	</article>
+</main>
